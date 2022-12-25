c30213e695e4649324a52202e6c5cd1a8064ff11
徐國堂 <roberthsu2003@gmail.com>
2022-12-25 16:07:51 +0800

    Update main.c

--- content/lesson2_2/main.c
+++ content/lesson2_2/main.c
@@ -5,7 +5,8 @@ int main(void) {
   int y = 210;
   int sum = x + y; // x的型別,運算時,自動轉換為int
   printf("sum值是:%d\n", sum);
-  double average = sum / 2.0;
+  //double average = sum / 2.0;
+	double average = (double)sum / 2; //強迫轉換
   printf("平均是:%.2lf\n", average);
   return 0;
 }
