64bdc4c1158a6468e51584e053e3c9aab6ea34ea
徐國堂 <roberthsu2003@gmail.com>
2022-12-25 15:10:57 +0800

    Update main.c

--- content/lesson2_2/main.c
+++ content/lesson2_2/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  int chinese;
-  int  english;
-  int math;
+  short chinese;
+  short english;
+  short math;
   printf("請輸入國文分數:");
-  scanf("%d", &chinese);
+  scanf("%hi", &chinese);
   printf("請輸入英文分數:");
-  scanf("%d", &english);
+  scanf("%hi", &english);
   printf("請輸入數學分數:");
-  scanf("%d", &math);
+  scanf("%hi", &math);
 
-  int sum = chinese + english + math;
+  short sum = chinese + english + math;
 
-  printf("國文分數%d\n", chinese);
-  printf("英文分數%d\n", english);
-  printf("數學分數%d\n", math);
-  printf("總分是%d\n", sum);
+  printf("國文分數%hi\n", chinese);
+  printf("英文分數%hi\n", english);
+  printf("數學分數%hi\n", math);
+  printf("總分是%hi\n", sum);
   return 0;
 }
