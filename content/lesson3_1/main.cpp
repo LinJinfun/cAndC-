5446ddf06f1579eb44a725724a29a03a7824c76d
徐國堂 <roberthsu2003@gmail.com>
2023-01-08 10:24:28 +0800

    Update main.cpp

--- content/lesson3_1/main.cpp
+++ content/lesson3_1/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-using namespace std;
 int main() {
   int math;
-	/*
-  printf("請輸入數學分數:");
-  scanf("%d", &math);
-  printf("我的數學分數是:%d\n", math);
-	*/
+  /*
+printf("請輸入數學分數:");
+scanf("%d", &math);
+printf("我的數學分數是:%d\n", math);
+  */
 
-	cout << "請輸入數學分數:";
-	cin >> math;
-	cout << "我的數學分數是:" << math << endl;
+  std::cout << "請輸入數學分數:";
+  std::cin >> math;
+  std::cout << "我的數學分數是:" << math << std::endl;
 }
