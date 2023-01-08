ac99c06de5d2163933bd35922cf15a2e441c4989
徐國堂 <roberthsu2003@gmail.com>
2023-01-08 11:26:43 +0800

    Update main.cpp

--- content/lesson3_4/main.cpp
+++ content/lesson3_4/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
-
+//請使用者輸入一個任意數，程式會顯示此數的平方值及立方值
 int main() {
-	int x = 1;
-	//x = x + 1;
-	//x++;
-	//++x;
-	x += 5;  // 複合指定運算子
-	cout << x << endl;
+  int num, result;
+  cout << "請輸入任意整數:";
+  cin >> num;
+  result = num;
+  result *= num;
+  cout << "此數的平方是:" << result << endl;
+  result *= num;
+  cout << "此數的立方是:" << result << endl;
 }
