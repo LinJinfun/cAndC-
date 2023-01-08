6bb3348c020c55355d40bc6ba00d18ca6a394052
徐國堂 <roberthsu2003@gmail.com>
2023-01-08 10:11:58 +0800

    Update main.cpp

--- content/lesson3_1/main.cpp
+++ content/lesson3_1/main.cpp
@@ -2,7 +2,13 @@
 
 int main() {
   int math;
+	/*
   printf("請輸入數學分數:");
   scanf("%d", &math);
   printf("我的數學分數是:%d\n", math);
+	*/
+
+	std::cout << "請輸入數學分數:";
+	std::cin >> math;
+	std::cout << "我的數學分數是:" << math << std::endl;
 }
