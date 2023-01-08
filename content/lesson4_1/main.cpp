33dad6c9252ed9998b41446b36070c9efdb5453c
徐國堂 <roberthsu2003@gmail.com>
2023-01-08 14:12:43 +0800

    Create main.cpp

---
+++ content/lesson4_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int scores;
+	bool add;
+  cout << "請輸入學生的分數:";
+  cin >> scores;
+	cout << "學生符合加分條件嗎? yes請輸入1,no請輸入0:";
+	cin >> add;
+	if(add){
+		scores *= 1.05;
+		if(scores > 300){
+			scores = 300;
+		}
+	}
+	
+  cout << "學生分數是:" << scores << endl;
+}
