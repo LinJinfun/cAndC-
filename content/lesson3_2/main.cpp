4942a839a30982f60808d03080c4f4706f631928
徐國堂 <roberthsu2003@gmail.com>
2023-01-08 10:23:31 +0800

    Create main.cpp

---
+++ content/lesson3_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int math;
+  /*
+printf("請輸入數學分數:");
+scanf("%d", &math);
+printf("我的數學分數是:%d\n", math);
+  */
+
+  cout << "請輸入數學分數:";
+  cin >> math;
+  cout << "我的數學分數是:" << math << endl;
+}
