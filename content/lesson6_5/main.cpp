db35c9fb982b44c7bade3e4510e7e4a18203ed73
徐國堂 <roberthsu2003@gmail.com>
2023-01-15 16:24:27 +0800

    Update main.cpp

--- content/lesson6_5/main.cpp
+++ content/lesson6_5/main.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-	int total;
-	int stu1[5];
-	stu1[0] = 78;
-	stu1[1] = 84;
-	stu1[2] = 93;
-	stu1[3] = 97;
-	stu1[4] = 73;
-	total = stu1[0] + stu1[1] + stu1[2] + stu1[3] + stu1[4];
+	int total = 0;
+	int stu1[] = {78, 84, 93, 97, 73};
+
+	for(int i=0; i<5; i++){
+		total += stu1[i];
+	}
 
 	cout << "stu1的總分是:" << total << endl;
 }
