17d6da4421c12f540701310a94b4ffffea57b9dd
徐國堂 <roberthsu2003@gmail.com>
2023-01-15 10:06:22 +0800

    Create main.cpp

---
+++ content/lesson5_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//計算固定中的支出，媽媽每天會將家裡的花費記錄下來，並且計算本週的花費總和
+
+int main() {
+	int n;
+	int sum=0;
+	for(int i=1;i<=7;i++){
+		if(i==7){
+			cout << "請輸入星期日的支出:";
+		}else{
+			cout << "請輸入星期" << i << "的支出:";
+		}		
+		cin >> n;
+		sum += n;
+	}
+
+	cout << "本星期的支出為:" << sum << "元\n";
+}
