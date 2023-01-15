2d149ee8a9789c4db8643d4818e0590faa231449
徐國堂 <roberthsu2003@gmail.com>
2023-01-15 16:15:32 +0800

    Create main.cpp

---
+++ content/lesson6_5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int total;
+	int stu1[5];
+	stu1[0] = 78;
+	stu1[1] = 84;
+	stu1[2] = 93;
+	stu1[3] = 97;
+	stu1[4] = 73;
+	total = stu1[0] + stu1[1] + stu1[2] + stu1[3] + stu1[4];
+
+	cout << "stu1的總分是:" << total << endl;
+}
