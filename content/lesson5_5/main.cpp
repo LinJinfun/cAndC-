d2667c88ac9116baebc690c6454016d9c413ed0e
徐國堂 <roberthsu2003@gmail.com>
2023-01-15 11:05:47 +0800

    Create main.cpp

---
+++ content/lesson5_5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//小美是一位教師，請你以while迴圈方式為小美設計一個輸入成績的程式，如果輸入負數表示成績輸入結束，在輸入成績結束後顯示班上總成績及平均成績。
+int main() {
+	int score;
+	int sum = 0;
+	cout << "請輸入學生成績:";
+	cin >> score;
+
+	while(score>=0){
+		sum += score;
+		cout << "請輸入學生成績:";
+		cin >> score;
+	}
+
+	cout << "學生總成績為:" << sum << endl;
+}
