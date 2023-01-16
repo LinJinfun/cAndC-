8f96159e39f85f0dd6d3d10face93a66ad74df1c
徐國堂 <roberthsu2003@gmail.com>
2023-01-16 15:36:35 +0800

    Create main.c

---
+++ content/lesson1_3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("%d\n",5);
+	printf("%.2f\n",5.0);
+	printf("%s\n","robert");
+	printf("我的姓名是:%s,我國文分數是:%d,我的平均分數是:%.2f\n","徐國堂",78,75.6);
+  return 0;
+}
