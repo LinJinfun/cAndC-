37e7cf15a56ba6d6205d53004b3598e92ab028e9
徐國堂 <roberthsu2003@gmail.com>
2023-01-16 16:07:56 +0800

    Create main.c

---
+++ content/lesson1_4/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("234+456=%d\n",234+456); //int + int
+	printf("7/2=%d\n",7/2); //int / int
+	printf("7/2=%.2f\n",7/2.0); // int / double
+  return 0;
+}
