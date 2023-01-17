9716db312f4423789d188617a3d0b9424cbc91c5
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 16:27:50 +0800

    Update main.cpp

--- content/lesson2_7/main.cpp
+++ content/lesson2_7/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
   int chinese,english,math,sum;
 	double average;
-  printf("請輸入國文,英文,數學(國文,英文,數學):");
+  cout << "請輸入國文,英文,數學(國文,英文,數學):";
 	scanf("%d,%d,%d",&chinese,&english,&math);
 
 	sum = chinese + english + math;
 	//average = (double)sum/3; //強迫轉換
 	average = sum/3.0; //自動轉換
-	printf("您的總分是%d分,平均為%.2f\n",sum,average);
+	cout << "您的總分是"<< sum << "分,";
+	cout << "平均為" << average << endl;
   return 0;
 }
