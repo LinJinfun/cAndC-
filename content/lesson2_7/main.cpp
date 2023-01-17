c56aa931b71117052fb8a7ab04aa5d6a5fe93d83
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 16:43:21 +0800

    Update main.cpp

--- content/lesson2_7/main.cpp
+++ content/lesson2_7/main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 int main() {
   int chinese,english,math,sum;
 	double average;
+	const char* name = "徐國堂"; //c語言的字元陣列
   cout << "請輸入國文,英文,數學(國文 英文 數學):";
 	cin >> chinese >> english >> math;
 
 	sum = chinese + english + math;
 	//average = (double)sum/3; //強迫轉換
 	average = sum/3.0; //自動轉換
-	cout << "您的總分是"<< sum << "分,";
-	cout << "平均為" << average << endl;
+	cout << "學生姓名:" << name << endl;
+	cout << "總分是"<< sum << "分";
+	cout << ",平均為" << average << endl;
   return 0;
 }
