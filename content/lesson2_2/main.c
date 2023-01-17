655995748ba18806417ada130436f10cf33b3712
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 14:50:35 +0800

    Create main.c

---
+++ content/lesson2_2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  char i = 97;
+	printf("%d\n",i);
+	printf("%c\n",i);
+
+	//儲存字元
+	char j = 'A';
+	printf("%d\n",j);
+	printf("%c\n",j);
+
+	//儲存數值
+	unsigned char m = 255;
+	printf("%d\n",m);
+  return 0;
+}
