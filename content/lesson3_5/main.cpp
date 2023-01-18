ac5414ceb4e26552a5433b90f691c4451519a325
徐國堂 <roberthsu2003@gmail.com>
2023-01-18 15:16:52 +0800

    Create main.cpp

---
+++ content/lesson3_5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//讓使用者輸入三個任意數，程式會顯示三數中的最大數。
+
+int main() {
+	int input1,input2,input3,max;
+	cout << "請輸入第一個數:";
+	cin >> input1;
+	cout << "請輸入第二個數:";
+	cin >> input2;
+	max = input1 > input2 ? input1 : input2;
+	cout << "請輸入第三個數:";
+	cin >> input3;
+	max = max > input3 ? max : input3;
+
+	cout << "輸入三個數中最大的數為:" << max << endl;
+}
