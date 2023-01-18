e062c99e6a04e717fd79ba676e37c4a0e9d7f951
徐國堂 <roberthsu2003@gmail.com>
2023-01-18 16:35:30 +0800

    Update main.cpp

--- content/lesson3_9/main.cpp
+++ content/lesson3_9/main.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 
 int main() {
-	int pressure;
-	cout << "請輸入血壓值:";
-	cin >> pressure;
-	if(pressure >= 70){
-		if(pressure <= 140){
-			cout << "血壓正常\n";
-		}else{
-			cout << "血壓太高\n";
-		}
-	}else{
-		cout << "血壓太低\n";
-	}
+  /*
+  int pressure;
+  cout << "請輸入血壓值:";
+  cin >> pressure;
+  if(pressure >= 70){
+          if(pressure <= 140){
+                  cout << "血壓正常\n";
+          }else{
+                  cout << "血壓太高\n";
+          }
+  }else{
+          cout << "血壓太低\n";
+  }
+  */
+
+  int pressure;
+  cout << "請輸入血壓值:";
+  cin >> pressure;
+  if (pressure >= 70 && pressure <= 140) {
+    cout << "血壓正常\n";
+  } else {
+    cout << "血壓不正常\n";
+  }
 }
