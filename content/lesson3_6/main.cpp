160b8c1ddd941f4fefb9e9bec42fc75257237a33
徐國堂 <roberthsu2003@gmail.com>
2023-01-18 15:24:00 +0800

    Create main.cpp

---
+++ content/lesson3_6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//讓使用者輸入梯形的上底、下底及高，程式會計算梯形的面積(上底加下底乘以高除以2)
+
+int main() {
+	float top,bottom,height;
+	cout << "請輸入梯形的上底(公分):";
+	cin >> top;
+	cout << "請輸入梯形的下底(公分):";
+	cin >> bottom;
+	cout << "請輸入梯形的高(公分):";
+	cin >> height;
+
+	cout << "梯形的面積:" << area << "平方公分\n";
+	
+}
