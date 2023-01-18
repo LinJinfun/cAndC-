98d6086dabf04a1d1e8f8718c171ad094bcdbda4
徐國堂 <roberthsu2003@gmail.com>
2023-01-18 16:24:54 +0800

    Create main.cpp

---
+++ content/lesson3_9/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int pressure;
+	cout << "請輸入血壓值:";
+	cin >> pressure;
+	if(pressure >= 70){
+		if(pressure <= 140){
+			cout << "血壓正常\n";
+		}else{
+			cout << "血壓太高\n";
+		}
+	}else{
+		cout << "血壓太低\n";
+	}
+}
