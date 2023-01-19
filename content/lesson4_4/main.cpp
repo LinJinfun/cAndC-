73b8a3f13c26ba8bb6cd2aca5ebe61138cbaabee
徐國堂 <roberthsu2003@gmail.com>
2023-01-19 15:06:56 +0800

    Create main.cpp

---
+++ content/lesson4_4/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//讓使用者輸入加、減、乘、除運算子, 就會顯示運算結果
+using namespace std;
+int main() {
+	int num1 = 20;
+	int num2 = 4;
+	char op;
+	cout << "num1=20,num2=4" << endl;
+	cout << "請輸入要執行的運算(+-*/):";
+	cin >> op;
+
+	switch(op){
+		case '+':
+			cout << "num1+num2=" << num1+num2 << endl;
+			break;
+		case '-':
+			cout << "num1-num2=" << num1-num2 << endl;
+			break;
+		case '*':
+			cout << "num1*num2=" << num1*num2 << endl;
+			break;
+		case '/':
+			cout << "num1/num2=" << num1/num2 << endl;
+			break;
+		default:
+			cout << "無法執行運算!" << endl;
+	}
+}
