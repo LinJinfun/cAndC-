d83807770e9e29b0e8476a85e6b2559c756a10f1
徐國堂 <roberthsu2003@gmail.com>
2023-01-19 15:58:07 +0800

    Update main.cpp

--- content/lesson4_6/main.cpp
+++ content/lesson4_6/main.cpp
@@ -8,8 +8,11 @@ int main() {
 	cin >> m;
 	
 	int total = 0;
+	int count = 0;
 	for(int i=2; i<=m; i+=2){
+		count++;
 		total += i;
 	}
+	cout << "總共執行:" << count << "次\n";
 	cout << "2~" << m << "的偶數總和為:" << total << endl;
 }
