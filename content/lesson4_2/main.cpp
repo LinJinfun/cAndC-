f4c965278e52e0011b24abe327760110db480ca3
徐國堂 <roberthsu2003@gmail.com>
2023-01-19 14:41:21 +0800

    Update main.cpp

--- content/lesson4_2/main.cpp
+++ content/lesson4_2/main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int main() {
 	int money;
 	int payMoney;
+	int discount;
 	
 	cout << "請輸入購買金額:";
 	cin >> money;
+	//滿1000送100
+	discount = money / 1000 * 100;
 
 	if(money < 10000){
 		payMoney = money;
@@ -21,6 +24,7 @@ int main() {
 	}else{
 		payMoney = money * 0.8;
 	}
-
+	
+	payMoney -= discount;
 	cout << "實付金額是:" << payMoney << "元\n";
 }
